a1719348418fc9bc7b887b7ad23a78c9668dc066
Daniel Park <intheimageofyeshua@gmail.com>
2025-02-08 21:17:54 +0900

    Added Font Draw

--- main.rb
+++ main.rb
@@ -8,13 +8,17 @@ class Game < Gosu::Window
   def initialize
     super(WIDTH, HEIGHT)
     self.caption = TITLE
-    @name = "daniel"
+    @background = Gosu::Image.new("images/background.png")
+    @bg_color = Gosu::Color.new(255, 0, 0, 0)
+    @text = Gosu::Image.from_text("Gosu", 80, font: "fonts/freesansbold.ttf")
   end
 
   def button_down(id)
     case id
     when Gosu::KB_ESCAPE
       close
+    when Gosu::KB_SPACE
+      rand_color
     end
   end
 
@@ -22,6 +26,15 @@ class Game < Gosu::Window
   end
 
   def draw
+    Gosu.draw_rect(0, 0, self.width, self.height, @bg_color)
+    @background.draw(0, 0)
+    @text.draw(0, 0)
+  end
+  
+  def rand_color
+    @bg_color.red = rand(256)
+    @bg_color.green = rand(256)
+    @bg_color.blue = rand(256)
   end
 end
 
